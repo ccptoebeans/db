87e8c5c7faba1ca405c6f2bc0489cba96815a929
CCP Cookies <69311849+ccpcookies@users.noreply.github.com>
2023-07-26 10:57:46 +0000

    Allow test_db tests a way to run if env var present
    Tests are skipped unless a connection string is supplied in the env
    var CCP_LOCALDB_CONNECTION_STRING. Change made to allow a way to run
    tests without edits to script.

--- tests/python/dbTest/test_db.py
+++ tests/python/dbTest/test_db.py
@@ -4,16 +4,8 @@ import sys
 import db
 import os
 
-CONNECTION_INFO = {
-    "uid": "zzp_user",
-    "database": "NOTSET",
-    "application_name": "eveDB2 test OLE DB",
-    "server": "SqlDev1IS",
-    "pwd": "zzp_user",
-    "provider": "MSOLEDBSQL"
-}
 
-CONNECTION_STRING = ""
+CONNECTION_STRING = os.environ.get('CCP_LOCALDB_CONNECTION_STRING', None)
 
 class Schema:
     def __init__(self):
@@ -30,18 +22,19 @@ class Schema:
 class DbUnitTests(unittest.TestCase):
 
     def setUp(self):
-        self.session = db.NSession(CONNECTION_STRING)
+        if CONNECTION_STRING != None:
+            self.session = db.NSession(CONNECTION_STRING)
         print("Set up done")
 
     def tearDown(self):
         print("Done")
 
-    @unittest.skip("Requires localdb")
+    @unittest.skipIf(not CONNECTION_STRING, "Skipping because it requires a local db") 
     def testGetSchema(self):
         schema = self.session.GetSchema(False)
         self.assertTrue(len(schema) > 0)
 
-    @unittest.skip("Requires localdb")
+    @unittest.skipIf(not CONNECTION_STRING, "Skipping because it requires a local db") 
     def testExcuteProcWithStrArgument(self):
         self.session.allowSync = 1
         procs, tables = self.session.GetSchema(False)
@@ -57,14 +50,14 @@ class DbUnitTests(unittest.TestCase):
         ret = self.session.Execute(zsystemSQLProcName, ["SELECT a = NULL"])
         self.assertTrue(ret != None)    
 
-    @unittest.skip("Requires localdb")
+    @unittest.skipIf(not CONNECTION_STRING, "Skipping because it requires a local db") 
     def testGetSessionStatus(self):
         sessionStatus = self.session.GetSessionStatus()
         self.assertEqual(sessionStatus['sessionCount'], 1)
         self.assertEqual(sessionStatus['sessionsInUse'], 0)
         self.assertEqual(sessionStatus['freeSessions'], 1)
 
-    @unittest.skip("Requires localdb")
+    @unittest.skipIf(not CONNECTION_STRING, "Skipping because it requires a local db") 
     def testGetSessionSettings(self):
         sessionsettings = self.session.GetSessionSettings()
         self.assertTrue(isinstance(sessionsettings['cleanEvery'], float))
@@ -72,7 +65,7 @@ class DbUnitTests(unittest.TestCase):
         self.assertTrue(isinstance(sessionsettings['maxFreeSessions'], int))
         self.assertTrue(isinstance(sessionsettings['minFreeSessions'], int))
 
-    @unittest.skip("Requires localdb")
+    @unittest.skipIf(not CONNECTION_STRING, "Skipping because it requires a local db") 
     def testSetSessionSettings(self):
         #NOTE: Passed in values are not currently type checked
         targetCleanEveryValue = 11.0
@@ -89,22 +82,4 @@ class DbUnitTests(unittest.TestCase):
         self.assertEqual(sessionsettings['minFreeSessions'], targetMinFreeSessions)
 
 if __name__ == '__main__':
-
-    # Parse arguments from environment variables
-    CONNECTION_INFO["uid"] = os.environ.get('UID', CONNECTION_INFO["uid"])
-    CONNECTION_INFO["database"] = os.environ.get('DATABASE', CONNECTION_INFO["database"])
-    CONNECTION_INFO["application_name"] = os.environ.get('APPLICATION_NAME', CONNECTION_INFO["application_name"])
-    CONNECTION_INFO["server"] = os.environ.get('SERVER', CONNECTION_INFO["server"])
-    CONNECTION_INFO["pwd"] = os.environ.get('PWD', CONNECTION_INFO["pwd"])
-    CONNECTION_INFO["provider"] = os.environ.get('PROVIDER', CONNECTION_INFO["provider"])
-
-    CONNECTION_STRING = "uid={0};database={1};application name={2};server={3};pwd={4};provider={5}".format(
-        CONNECTION_INFO["uid"],
-        CONNECTION_INFO["database"],
-        CONNECTION_INFO["application_name"],
-        CONNECTION_INFO["server"],
-        CONNECTION_INFO["pwd"],
-        CONNECTION_INFO["provider"]
-    )
-
     unittest.main()
