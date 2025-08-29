a3b03e22d5472bd3c8530422bb0e9b0a97c9d82f
ccp serpent <162123512+ccp-serpent@users.noreply.github.com>
2025-08-29 12:53:41 +0000

    Control build parallelism using environment
    This allows us to control parallelism on a per-build basis.

--- .teamcity/Windows/Project.kt
+++ .teamcity/Windows/Project.kt
@@ -60,6 +60,8 @@ class CarbonBuildWindows(buildName: String, configType: String, preset: String) 
         param("env.CMAKE_PRESET", preset)
         param("env.VCPKG_BINARY_SOURCES", "clear;x-aws,s3://vcpkg-binary-cache-static/cache/,readwrite")
         param("env.X_VCPKG_REGISTRIES_CACHE", "%teamcity.build.checkoutDir%/%github_checkout_folder%/regcache")
+        param("env.CMAKE_BUILD_PARALLEL_LEVEL", "8")
+        param("env.CTEST_PARALLEL_LEVEL", "8")
     }
 
     vcs {
@@ -102,13 +104,13 @@ class CarbonBuildWindows(buildName: String, configType: String, preset: String) 
         exec {
             name = "Build"
             path = "cmake"
-            arguments = "--build %env.CMAKE_BUILD_FOLDER% --config %env.CMAKE_CONFIG_TYPE% --target %env.CMAKE_BUILD_TARGETS% --parallel 8"
+            arguments = "--build %env.CMAKE_BUILD_FOLDER% --config %env.CMAKE_CONFIG_TYPE% --target %env.CMAKE_BUILD_TARGETS%"
         }
         exec {
             name = "Run Tests"
             workingDir = "%env.CMAKE_BUILD_FOLDER%"
             path = "ctest"
-            arguments = "-C %env.CMAKE_CONFIG_TYPE% -V --output-on-failure --output-junit %env.CTEST_JUNIT_OUTPUT_FILE% -j 8"
+            arguments = "-C %env.CMAKE_CONFIG_TYPE% -V --output-on-failure --output-junit %env.CTEST_JUNIT_OUTPUT_FILE%"
         }
         exec {
             name = "Package artifact"
